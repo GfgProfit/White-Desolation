beaa3584335d23fdacf60b0b22238ea52885d557
Bogdan <155587385+GfgProfit@users.noreply.github.com>
2026-04-24 16:34:00 +0500

    Remove FireSystem UI and scripts
    Remove the FireSystem feature: delete FireSystem scripts and their .meta files (FireItemsConfig, FireProgressView, FireSourceInteractable, FireStartingChoiceView, FireStartingUIController and FireSystem folder meta). Clean up prefabs and scene references by stripping FireStartingChoiceView/FireProgressView/FireStartingUIController/FireSourceInteractable components from Fire Choice.prefab and Assets/Scenes/TestScene.unity. Update TestSceneInstaller to remove the serialized FireStartingUIController field and its container binding. This cleans up missing/unused component references after removing the fire-starting UI/system implementation.

--- Assets/_Source/Scripts/DI/Installers/TestSceneInstaller.cs
+++ Assets/_Source/Scripts/DI/Installers/TestSceneInstaller.cs
@@ -3,13 +3,11 @@ using UnityEngine;
 public class TestSceneInstaller : SceneInstaller
 {
     [SerializeField] private InventoryController _inventoryController;
-    [SerializeField] private FireStartingUIController _fireStartingUIController;
 
     protected override void Install(IContainer container)
     {
         container.Bind<IPlayerInput>(_ => new LegacyPlayerInput()).AsSingle();
 
         container.BindInstance(_inventoryController).AsSingle();
-        container.BindInstance(_fireStartingUIController).AsSingle();
     }
 }
